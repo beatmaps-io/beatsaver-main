36d05a9bb1153888b1c3ee8cd1347ae9b5d32495
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-06-11 21:42:32 +0100

    Refactoring min-width

--- src/jsMain/kotlin/io/beatmaps/nav/viewport-min-width.kt
+++ src/jsMain/kotlin/io/beatmaps/nav/viewport-min-width.kt
@@ -7,15 +7,24 @@ fun viewportMinWidthPolyfill() {
     document.querySelector("meta[name=viewport]")?.let { viewport ->
         val content = viewport.getAttribute("content") ?: ""
         val minWidthPart = content.split(",").map { it.trim().split("=") }.firstOrNull { it[0] === "min-width" }
+        var currentState = false
         minWidthPart?.get(1)?.toInt()?.let { minWidth ->
             fun updateViewportSize() {
-                if (window.screen.width < minWidth) {
-                    document.head?.removeChild(viewport)
+                if (window.screen.width < minWidth == currentState)
+                    return
 
+                currentState = window.screen.width < minWidth
+                document.querySelector("meta[name=viewport]")?.let { currentViewport ->
+                    document.head?.removeChild(currentViewport)
+                }
+
+                if (window.screen.width < minWidth) {
                     val newViewport = document.createElement("meta")
                     newViewport.setAttribute("name", "viewport")
                     newViewport.setAttribute("content", "width=$minWidth")
                     document.head?.appendChild(newViewport)
+                } else {
+                    document.head?.appendChild(viewport)
                 }
             }
             window.onresize = { updateViewportSize() }
