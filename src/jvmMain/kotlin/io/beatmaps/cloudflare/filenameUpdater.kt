c71a63d835074a1474be979b559d626adc1de1a9
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-05-16 12:46:06 +0100

    Update filenames in R2

---
+++ src/jvmMain/kotlin/io/beatmaps/cloudflare/filenameUpdater.kt
@@ -0,0 +1,108 @@
+package io.beatmaps.cloudflare
+
+import io.beatmaps.common.CDNUpdate
+import io.beatmaps.common.consumeAck
+import io.beatmaps.common.dbo.Versions
+import io.beatmaps.common.dbo.VersionsDao
+import io.beatmaps.common.downloadFilename
+import io.beatmaps.common.localFolder
+import io.beatmaps.common.rabbitOptional
+import io.ktor.application.Application
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+import java.io.File
+import java.util.logging.Logger
+
+val cloudflareAccountId = System.getenv("CF_ACC_ID") ?: ""
+val cloudflareAuthToken = System.getenv("CF_AUTH_TOKEN") ?: ""
+val cloudflareKVId = System.getenv("CF_KV_ID") ?: ""
+
+val cloudflareR2Key = System.getenv("CF_R2_KEY") ?: ""
+val cloudflareR2Secret = System.getenv("CF_R2_SECRET") ?: ""
+val cloudflareR2Bucket = System.getenv("CF_R2_BUCKET") ?: "beatsaver"
+
+private val logger = Logger.getLogger("bmio.Cloudflare")
+
+fun Application.filenameUpdater() {
+    if (cloudflareAccountId.isEmpty()) {
+        logger.warning("Cloudflare updater not set up")
+        return
+    }
+
+    val worker = Worker(cloudflareAccountId, cloudflareAuthToken)
+    val beatsaverKVStore = worker.getKVStore(cloudflareKVId)
+    val r2Client = R2(cloudflareAccountId, cloudflareR2Key, cloudflareR2Secret).getBucket(cloudflareR2Bucket)
+
+    rabbitOptional {
+        consumeAck("cdn.r2", CDNUpdate::class) { _, update ->
+            if (update.deleted) {
+                deleteFromR2(update, r2Client)
+
+                return@consumeAck
+            }
+
+            val hash = update.hash ?: return@consumeAck
+
+            updateDownloadFilename(update, beatsaverKVStore, hash)
+            //uploadToR2(update, r2Client)
+        }
+    }
+}
+
+private suspend fun updateDownloadFilename(update: CDNUpdate, beatsaverKVStore: IKVStore, hash: String) {
+    val updateSongName = update.songName
+    val updateLevelAuthorName = update.levelAuthorName
+
+    if (updateSongName != null && updateLevelAuthorName != null) {
+        beatsaverKVStore.setValue(
+            hash,
+            downloadFilename(Integer.toHexString(update.mapId), updateSongName, updateLevelAuthorName)
+        )
+    }
+}
+
+private fun uploadToR2(update: CDNUpdate, r2Client: IR2Bucket) {
+    val toUpload = transaction {
+        VersionsDao.wrapRows(
+            Versions.select {
+                (Versions.r2 neq true) and (Versions.mapId eq update.mapId)
+            }
+        ).map { it.hash }
+    }
+
+    toUpload.map { File(localFolder(it), "${it}.zip") }.filter { it.exists() }.map {
+        r2Client.uploadFile(it)
+    }
+
+    transaction {
+        Versions.update({
+            (Versions.hash inList toUpload) and (Versions.mapId eq update.mapId)
+        }) {
+            it[r2] = true
+        }
+    }
+}
+
+private fun deleteFromR2(update: CDNUpdate, r2Client: IR2Bucket) {
+    val toDelete = transaction {
+        VersionsDao.wrapRows(
+            Versions.select {
+                (Versions.r2 eq true) and (Versions.mapId eq update.mapId)
+            }
+        ).map { it.hash }
+    }
+
+    toDelete.forEach {
+        r2Client.deleteFile("${it}.zip")
+    }
+
+    transaction {
+        Versions.update({
+            (Versions.hash inList toDelete) and (Versions.mapId eq update.mapId)
+        }) {
+            it[r2] = false
+        }
+    }
+}
---
+++ src/jvmMain/kotlin/io/beatmaps/cloudflare/worker.kt
@@ -0,0 +1,58 @@
+package io.beatmaps.cloudflare
+
+import io.beatmaps.common.client
+import io.ktor.client.features.HttpRequestTimeoutException
+import io.ktor.client.features.timeout
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.put
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+interface IKVStore {
+    suspend fun getKeys(): List<String>
+    suspend fun setValue(key: String, value: String)
+    suspend fun setValues(kvs: List<KeyValue>)
+}
+
+private data class KeyResponse(val success: Boolean, val result: List<Key>)
+private data class Key(val name: String)
+data class KeyValue(val key: String, val value: String)
+
+private fun requestCommon(builder: HttpRequestBuilder, authToken: String) {
+    builder.header("Authorization", "Bearer $authToken")
+    builder.timeout {
+        socketTimeoutMillis = 5000
+        requestTimeoutMillis = 20000
+    }
+}
+
+private class KVStore(val worker: Worker, val namespaceId: String): IKVStore {
+    @Throws(HttpRequestTimeoutException::class)
+    override suspend fun getKeys() = client.get<KeyResponse>("https://api.cloudflare.com/client/v4/accounts/${worker.accountId}/storage/kv/namespaces/$namespaceId/keys") {
+        requestCommon(this, worker.authToken)
+    }.result.map { it.name }
+
+    @Throws(HttpRequestTimeoutException::class)
+    override suspend fun setValue(key: String, value: String) {
+        client.put<String>("https://api.cloudflare.com/client/v4/accounts/${worker.accountId}/storage/kv/namespaces/$namespaceId/values/$key") {
+            requestCommon(this, worker.authToken)
+            body = value
+        }
+    }
+
+    @Throws(HttpRequestTimeoutException::class)
+    override suspend fun setValues(kvs: List<KeyValue>) {
+        client.put<String>("https://api.cloudflare.com/client/v4/accounts/${worker.accountId}/storage/kv/namespaces/$namespaceId/bulk") {
+            requestCommon(this, worker.authToken)
+            contentType(ContentType.Application.Json)
+            body = kvs
+        }
+    }
+
+}
+
+class Worker(val accountId: String, val authToken: String) {
+    fun getKVStore(namespaceId: String): IKVStore = KVStore(this, namespaceId)
+}
