1e392e869f4a6d3cafa5f2024918bb58dcb02e2f
Pixelguymm <maartenmarx@hotmail.be>
2022-07-03 23:44:17 +0200

    Update migration number

--- src/commonMain/resources/db/migration/V22_UnsureToBot.sql
+++
@@ -1,8 +0,0 @@
-UPDATE beatmap
-    SET automapper = TRUE
-    FROM
-        beatmap AS b LEFT JOIN versions AS v ON b."mapId" = v."mapId"
-    WHERE
-        b."mapId" = beatmap."mapId"
-        AND state = 'Published'
-        AND "sageScore" < 0;
