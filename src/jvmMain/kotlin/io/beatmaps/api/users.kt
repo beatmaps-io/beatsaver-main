e954dbac20dfd9074aabe3f5b6b7a6ff58ae844c
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-09-30 23:53:28 +0100

    md5

--- src/jvmMain/kotlin/io/beatmaps/api/users.kt
+++ src/jvmMain/kotlin/io/beatmaps/api/users.kt
@@ -87,7 +87,7 @@ import java.time.temporal.ChronoUnit
 import java.util.Base64
 import java.util.Date
 
-fun md5(input:String): String {
+fun md5(input: String): String {
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
 }
