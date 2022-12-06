8d2627c6eba744fb91af8b6f61dc86ad8148fe28
Thomas Cheyney <top_cat@thomasc.co.uk>
2022-12-06 11:25:27 +0000

    Revert using router for profile links

--- src/jsMain/kotlin/io/beatmaps/util/markdown.kt
+++ src/jsMain/kotlin/io/beatmaps/util/markdown.kt
@@ -26,7 +26,7 @@ fun String.parseMapReference() =
 
 fun String.parseUserReference() =
     replace("(^|\\s)@([\\w.-]+?)($|[^\\w.-])".toRegex(setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))) {
-        """${it.groupValues[1]}<a data-bs="local" href="/profile/username/${it.groupValues[2].lowercase()}">@${it.groupValues[2]}</a>${it.groupValues[3]}"""
+        """${it.groupValues[1]}<a href="/profile/username/${it.groupValues[2].lowercase()}">@${it.groupValues[2]}</a>${it.groupValues[3]}"""
     }
 
 fun String.parseSocialLinks() =
