523a9d7d0608fb14f0f9f80d70ef32cd580cda5c
Pixelguymm <maartenmarx@hotmail.be>
2023-01-06 15:56:55 +0100

    Add unique index for bookmark playlists

--- src/commonMain/resources/db/migration/V41__BookmarksIndex.sql
+++ src/commonMain/resources/db/migration/V41__BookmarksIndex.sql
@@ -3,3 +3,7 @@ CREATE INDEX IF NOT EXISTS playlist_type
         ("createdAt" ASC NULLS LAST, "type")
     TABLESPACE pg_default
     WHERE "type" != 'Private' AND "deletedAt" IS NULL;
+
+CREATE UNIQUE INDEX IF NOT EXISTS bookmark_playlist
+    ON public.playlist USING btree ("owner")
+    WHERE "type" = 'System';
