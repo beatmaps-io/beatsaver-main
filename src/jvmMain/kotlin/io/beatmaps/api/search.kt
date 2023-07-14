db4603a0b02c00586b3c736ca4e1ec39dfd8cf11
Thomas Cheyney <top_cat@thomasc.co.uk>
2023-07-14 15:18:46 +0100

    Always use cross join for versions

--- src/jvmMain/kotlin/io/beatmaps/api/search.kt
+++ src/jvmMain/kotlin/io/beatmaps/api/search.kt
@@ -204,23 +204,20 @@ fun Route.searchRoute() {
                 .select {
                     Beatmap.id.inSubQuery(
                         Beatmap
-                            .joinUploader()
-                            .let { q ->
-                                if (needsDiff) {
-                                    q.crossJoin(
-                                        Versions
-                                            .join(Difficulty, JoinType.INNER, Versions.id, Difficulty.versionId)
-                                            .slice(intLiteral(1))
-                                            .select {
-                                                (Versions.mapId eq Beatmap.id) and (Versions.state eq EMapState.Published)
-                                                    .notNull(it.minNps) { o -> (Difficulty.nps greaterEqF o) }
-                                                    .notNull(it.maxNps) { o -> (Difficulty.nps lessEqF o) }
-                                            }
-                                            .limit(1)
-                                            .lateral().alias("diff")
-                                    )
-                                } else q
-                            }
+                            .crossJoin(
+                                Versions
+                                    .let { q ->
+                                        if (needsDiff) q.join(Difficulty, JoinType.INNER, Versions.id, Difficulty.versionId) else q
+                                    }
+                                    .slice(intLiteral(1))
+                                    .select {
+                                        (Versions.mapId eq Beatmap.id) and (Versions.state eq EMapState.Published)
+                                            .notNull(it.minNps) { o -> (Difficulty.nps greaterEqF o) }
+                                            .notNull(it.maxNps) { o -> (Difficulty.nps lessEqF o) }
+                                    }
+                                    .limit(1)
+                                    .lateral().alias("diff")
+                            )
                             .slice(Beatmap.id)
                             .select {
                                 Beatmap.deletedAt.isNull()
