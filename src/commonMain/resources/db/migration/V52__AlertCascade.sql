04dd34f2f1239bc1ec8bfee3ad7e564f55815e60
Pixelguymm <maartenmarx@hotmail.be>
2023-08-15 17:12:46 +0200

    Remove unnecessary DB migration

--- src/commonMain/resources/db/migration/V52__AlertCascade.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.alert_recipient
-    DROP CONSTRAINT alert_fk,
-    ADD CONSTRAINT alert_fk FOREIGN KEY ("alertId")
-        REFERENCES public.alert ("alertId") MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
