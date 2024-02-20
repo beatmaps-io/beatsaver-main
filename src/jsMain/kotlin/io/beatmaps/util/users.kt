994c1c238c7d656e3442ae97e22c6f9e45698e8b
Pixelguymm <maartenmarx@hotmail.be>
2024-02-20 15:10:44 +0100

    Lint again?

--- src/jsMain/kotlin/io/beatmaps/util/users.kt
+++ src/jsMain/kotlin/io/beatmaps/util/users.kt
@@ -4,8 +4,11 @@ import io.beatmaps.api.UserDetail
 
 fun userTitles(u: UserDetail?) = listOfNotNull(
     if (u?.admin == true) "Admin" else null,
-    if (u?.seniorCurator == true) "Senior Curator" else
-        { if (u?.curator == true) "Curator" else null },
+    if (u?.seniorCurator == true) {
+        "Senior Curator"
+    } else {
+        if (u?.curator == true) "Curator" else null
+    },
     if (u?.patreon?.supporting == true) u.patreon.title else null,
     (if (u?.verifiedMapper == true) "Verified " else "") +
         if (u?.stats?.totalMaps != 0) "Mapper" else "User"
