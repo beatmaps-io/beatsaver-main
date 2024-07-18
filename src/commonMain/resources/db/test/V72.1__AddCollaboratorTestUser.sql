c55a591fb933189a78e0185b4c00f451c282b32f
Timothy Gillespie <timothy@gillespie.eu>
2024-07-18 18:41:08 +0200

    Add two collaboration user for easier testing

---
+++ src/commonMain/resources/db/test/V72.1__AddCollaboratorTestUser.sql
@@ -0,0 +1,3 @@
+INSERT INTO uploader (name, email, password, active, testplay, admin, "uniqueName") VALUES
+    ('collab1', 'collab1@test.com', '$2a$12$fDDR0OkcN8PULvcCLt5aQOiGr9fOL3kMoOz2cs.vi0h.FE5MTJsNm', TRUE, FALSE, FALSE, 'collab1'),
+    ('collab2', 'collab2@test.com', '$2a$12$dBNyDjih/EZOy7Fro/yYF.9hP9vQ318xBybVFPAIwbbHVhJk12ySG', TRUE, FALSE, FALSE, 'collab2');
