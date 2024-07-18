30aa4394a7dd99f813fda9e0f921ea825e2c57cd
Timothy Gillespie <timothy@gillespie.eu>
2024-07-19 01:49:30 +0200

    Take first map instead of a set ID for setting up collaborations

--- src/commonMain/resources/db/test/V72.2__AddCollaboratorsToMaps.sql
+++ src/commonMain/resources/db/test/V72.2__AddCollaboratorsToMaps.sql
@@ -3,15 +3,24 @@ $$
 DECLARE
     collab1Id INTEGER;
     collab2Id INTEGER;
+    map1Id INTEGER;
+    map2Id INTEGER;
+    map3Id INTEGER;
+    map4Id INTEGER;
 BEGIN
     SELECT id INTO collab1Id FROM uploader WHERE "uniqueName" = 'collab1';
     SELECT id INTO collab2Id FROM uploader WHERE "uniqueName" = 'collab2';
 
+    SELECt "mapId" INTO map1Id FROM beatmap OFFSET 0 LIMIT 1;
+    SELECT "mapId" INTO map2Id FROM beatmap OFFSET 1 LIMIT 1;
+    SELECT "mapId" INTO map3Id FROM beatmap OFFSET 2 LIMIT 1;
+    SELECT "mapId" INTO map4Id FROM beatmap OFFSET 3 LIMIT 1;
+
     INSERT INTO collaboration ("mapId", "collaboratorId", "requestedAt", accepted, "uploadedAt") VALUES
-         (1, collab1Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
-         (2, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
-         (3, collab1Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
-         (3, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
-         (4, collab1Id, '2021-01-01 00:00:00', FALSE, '2021-01-01 00:00:00'),
-         (4, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00');
+         (map1Id, collab1Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
+         (map2Id, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
+         (map3Id, collab1Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
+         (map3Id, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00'),
+         (map4Id, collab1Id, '2021-01-01 00:00:00', FALSE, '2021-01-01 00:00:00'),
+         (map4Id, collab2Id, '2021-01-01 00:00:00', TRUE, '2021-01-01 00:00:00');
 END $$;
