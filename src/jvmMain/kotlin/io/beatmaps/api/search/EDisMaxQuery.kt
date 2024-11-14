2a00f5c691bd25033f711259abc2368dfbb9b778
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-11-14 23:15:11 +0000

    Add extra helper methods to dismax classes

--- src/jvmMain/kotlin/io/beatmaps/api/search/EDisMaxQuery.kt
+++ src/jvmMain/kotlin/io/beatmaps/api/search/EDisMaxQuery.kt
@@ -6,6 +6,36 @@ class EDisMaxQuery : DisMaxQuery() {
     }
 
     fun setBoost(field: SolrField<*>?) = this.also {
-        set("boost", field?.toText())
+        set(BOOST, field?.toText())
+    }
+
+    fun setSplitOnWhitespace(split: Boolean = true) = this.also {
+        set(SOW, split.toString())
+    }
+
+    fun setLowercaseOperators(lowercase: Boolean = true) = this.also {
+        set(LOWERCASE_OPERATORS, lowercase.toString())
+    }
+
+    fun setUserFields(allowedFields: Array<out SolrField<*>>? = arrayOf(), disallowedFields: Array<out SolrField<*>> = arrayOf()) = this.also {
+        val v = if (allowedFields?.isEmpty() == true && disallowedFields.isEmpty()) {
+            "-*"
+        } else {
+            "${allowedFields?.joinToString(" ") { it.toText() } ?: "*"} ${disallowedFields.joinToString(" ") { it.toText() }}"
+        }
+        set(USER_FIELDS, v)
+    }
+
+    fun setAllowedFields(vararg allowedFields: SolrField<*>) =
+        setUserFields(allowedFields)
+
+    fun setDisallowedFields(vararg disallowedFields: SolrField<*>) =
+        setUserFields(null, disallowedFields)
+
+    companion object {
+        const val BOOST = "boost"
+        const val SOW = "sow"
+        const val USER_FIELDS = "uf"
+        const val LOWERCASE_OPERATORS = "lowercaseOperators"
     }
 }
