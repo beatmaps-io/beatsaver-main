199e7d6ba029167b47392ade379ce0d3789d7ea1
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-11-18 21:31:58 +0000

    Use solr for search playlists

--- src/jvmMain/kotlin/io/beatmaps/api/search/BsSolr.kt
+++ src/jvmMain/kotlin/io/beatmaps/api/search/BsSolr.kt
@@ -1,5 +1,7 @@
 package io.beatmaps.api.search
 
+import io.beatmaps.common.SearchOrder
+
 object BsSolr : SolrCollection() {
     val author = string("author")
     val created = pdate("created")
@@ -32,4 +34,22 @@ object BsSolr : SolrCollection() {
     val authorEn = string("author_en")
     val nameEn = string("name_en")
     val descriptionEn = string("description_en")
+
+    // Weights
+    private val queryFields = arrayOf(
+        name to 4.0,
+        nameEn to 1.0,
+        author to 10.0,
+        authorEn to 2.0,
+        descriptionEn to 0.5
+    )
+
+    fun newQuery(sortOrder: SearchOrder) =
+        EDisMaxQuery()
+            .setBoost(if (sortOrder == SearchOrder.Relevance) voteScore else null)
+            .setQueryFields(*queryFields)
+            .setTie(0.1)
+            .setMinimumMatch(
+                PercentageMinimumMatchExpression(-0.5f)
+            )
 }
