48bcf5fddc68dd85c6208d959debe30d6c01924e
Thomas Cheyney <top_cat@thomasc.co.uk>
2025-01-03 19:31:44 +0000

    Fix weird radio button react bug

--- src/jsMain/kotlin/io/beatmaps/shared/form/multipleChoice.kt
+++ src/jsMain/kotlin/io/beatmaps/shared/form/multipleChoice.kt
@@ -1,10 +1,12 @@
 package io.beatmaps.shared.form
 
 import external.reactFor
+import kotlinx.html.INPUT
 import kotlinx.html.InputType
 import kotlinx.html.id
 import kotlinx.html.js.onChangeFunction
 import react.Props
+import react.dom.RDOMBuilder
 import react.dom.div
 import react.dom.input
 import react.dom.label
@@ -18,15 +20,21 @@ external interface MultipleChoiceProps<T> : Props {
     var className: String?
 }
 
+fun RDOMBuilder<INPUT>.betterChecked(b: Boolean) {
+    attrs.attributes["checked"] = if (b) "checked" else ""
+}
+
 val multipleChoice = fc<MultipleChoiceProps<Any?>>("multipleChoice") { props ->
     div("multiple-choice ${props.className ?: ""}") {
         props.choices.forEach { (text, value) ->
             val id = "${props.name}:${text.lowercase()}"
 
             input(InputType.radio, classes = "form-check-input") {
+                key = id
                 attrs.id = id
                 attrs.name = props.name
-                attrs.checked = value == props.selectedValue
+                // If you use the checked option react gets upset that the controlled state changes
+                betterChecked(value == props.selectedValue)
                 attrs.onChangeFunction = {
                     props.block?.invoke(value)
                 }
